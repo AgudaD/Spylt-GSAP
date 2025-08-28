490831ee166a1fa791d96ad4f610d15c7fef9a49
Aguda <agudaoluwanifemi@gmail.com>
2025-08-28 14:23:14 +0100

    nutri-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
-import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
-import Navbar from './components/Navbar'
-import HeroSection from './sections/HeroSection'
-import MessageSection from './sections/MessageSection'
-import FlavorSection from './sections/FlavorSection'
+import React from "react";
+import gsap from "gsap";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
+import NutritionSection from "./sections/NutritionSection";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 2,
+      effects: true,
+    });
+  });
+
   return (
     <main>
       <Navbar />
-      <HeroSection />
-      <MessageSection />
-      <FlavorSection />
-      <div className="h-dvh border border-red-500"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <NutritionSection />
+          <div className="h-dvh border border-red-500"></div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
